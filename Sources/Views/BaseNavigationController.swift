55d3211e0067fb3b73055f88107ce520d43db8ec
Clay Ellis <clayellis@me.com>
2017-11-20 22:27:08 -0700

    Add BaseNavigationController

---
+++ Sources/Views/BaseNavigationController.swift
@@ -0,0 +1,13 @@
+//
+//  BaseNavigationController.swift
+//  DWM
+//
+//  Created by Clay Ellis on 11/18/17.
+//  Copyright © 2017 Test. All rights reserved.
+//
+
+import UIKit
+
+final class BaseNavigationController: UINavigationController {
+    
+}
