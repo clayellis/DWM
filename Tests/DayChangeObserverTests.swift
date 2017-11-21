6c347545dc4fcde669c76b7daf734933edeb0a67
Clay Ellis <clayellis@me.com>
2017-11-20 21:02:22 -0700

    Break test Store out of DayChangeObserverTests into Utilities.swift and move tests into own test case class.

--- Tests/DayChangeObserverTests.swift
+++ Tests/DayChangeObserverTests.swift
@@ -11,66 +11,12 @@ import XCTest
 
 class DayChangeObserverTests: XCTestCase {
 
-    class Store: SimpleStoreProtocol {
-        var storage: [String: Any?] = [:]
-
-        func store(value: Any, withIdentifier identifier: String) {
-            storage[identifier] = value
-        }
-
-        func retrieveValue(withIdentifier identifier: String) -> Any? {
-            return storage[identifier] ?? nil
-        }
-
-        func deleteValue(withIdentifier identifier: String) {
-            storage.removeValue(forKey: identifier)
-        }
-    }
-
     var storage: SimpleStoreProtocol!
 
     override func setUp() {
         super.setUp()
         storage = Store()
     }
-
-    override func tearDown() {
-        super.tearDown()
-        (storage as! Store).storage = [:]
-    }
-
-    // MARK: Internal Store Tests
-
-    func testStorageBeginsEmpty() {
-        XCTAssert((storage as! Store).storage.count == 0)
-    }
-
-    func testStorageStoreValue() {
-        let value = 10
-        let identifier = "Number"
-        storage.store(value: value, withIdentifier: identifier)
-        let result = storage.retrieveValue(withIdentifier: identifier) as? Int
-        XCTAssertEqual(value, result)
-    }
-
-    func testStorageRetrieveMissingValue() {
-        storage.store(value: 10, withIdentifier: "Number")
-        let result = storage.retrieveValue(withIdentifier: "Not The Same")
-        XCTAssertNil(result)
-    }
-
-    func testStorageDeleteValue() {
-        let value = 10
-        let identifier = "Number"
-        storage.store(value: value, withIdentifier: identifier)
-        let storageResult = storage.retrieveValue(withIdentifier: identifier) as? Int
-        XCTAssertEqual(value, storageResult)
-        storage.deleteValue(withIdentifier: identifier)
-        let deleteResult = storage.retrieveValue(withIdentifier: identifier) as? Int
-        XCTAssertNil(deleteResult)
-    }
-
-    // MARK: Day Change Observer Tests
 
     func testObserverSetsLastObservedDateOnBeginObserving() {
         let timeEngine = TimeEngine(fixedNow: date(month: 1, day: 1))
@@ -123,3 +69,4 @@ class DayChangeObserverTests: XCTestCase {
         XCTAssertFalse(didChange)
     }
 }
+
--- Tests/Utilities.swift
+++ Tests/Utilities.swift
@@ -16,7 +16,6 @@ func date(month: Int, day: Int, year: Int? = nil, hour: Int = 0, calendar: Calen
 }
 
 class DateTests: XCTestCase {
-
     func testCorrectDate() {
         let components = DateComponents(year: 1993, month: 7, day: 14, hour: 3)
         let calendar = Calendar.current
@@ -35,5 +34,59 @@ class DateTests: XCTestCase {
         XCTAssertNotNil(control)
         XCTAssertNotNil(result)
         XCTAssertNotEqual(result, control)
+    }
+}
+
+class Store: SimpleStoreProtocol {
+    var storage: [String: Any?] = [:]
+
+    func store(value: Any, withIdentifier identifier: String) {
+        storage[identifier] = value
+    }
+
+    func retrieveValue(withIdentifier identifier: String) -> Any? {
+        return storage[identifier] ?? nil
+    }
+
+    func deleteValue(withIdentifier identifier: String) {
+        storage.removeValue(forKey: identifier)
+    }
+}
+
+class StoreTests: XCTestCase {
+    var storage: SimpleStoreProtocol!
+
+    override func setUp() {
+        super.setUp()
+        storage = Store()
+    }
+
+    func testStorageBeginsEmpty() {
+        XCTAssert((storage as! Store).storage.count == 0)
+    }
+
+    func testStorageStoreValue() {
+        let value = 10
+        let identifier = "Number"
+        storage.store(value: value, withIdentifier: identifier)
+        let result = storage.retrieveValue(withIdentifier: identifier) as? Int
+        XCTAssertEqual(value, result)
+    }
+
+    func testStorageRetrieveMissingValue() {
+        storage.store(value: 10, withIdentifier: "Number")
+        let result = storage.retrieveValue(withIdentifier: "Not The Same")
+        XCTAssertNil(result)
+    }
+
+    func testStorageDeleteValue() {
+        let value = 10
+        let identifier = "Number"
+        storage.store(value: value, withIdentifier: identifier)
+        let storageResult = storage.retrieveValue(withIdentifier: identifier) as? Int
+        XCTAssertEqual(value, storageResult)
+        storage.deleteValue(withIdentifier: identifier)
+        let deleteResult = storage.retrieveValue(withIdentifier: identifier) as? Int
+        XCTAssertNil(deleteResult)
     }
 }
