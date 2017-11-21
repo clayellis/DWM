3dede4ed946ee7e39a75b162f2ed0096c357a226
Clay Ellis <clayellis@me.com>
2017-11-21 14:30:58 -0700

    Add comments surrounding equality

--- Sources/Models/Task/Task.swift
+++ Sources/Models/Task/Task.swift
@@ -27,6 +27,11 @@ extension Task: Hashable {
     }
 
     static func == (lhs: Task, rhs: Task) -> Bool {
+        // FIXME: I'm not sure if every field should be compared to determine equality
+        // I think the hashValue of the index should be enough?
+        // But that doubt is coming up because I want to disallow duplicate titles.
+        // And really we should just disallow duplicate titles in the same list.
+        // Because sometimes a user might want "Sweep floors" in daily and monthly
         guard lhs.hashValue == rhs.hashValue else { return false }
         guard lhs.id == rhs.id else { return false }
         guard lhs.title == rhs.title else { return false }
