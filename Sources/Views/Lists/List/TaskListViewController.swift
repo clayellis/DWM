3e6f36cd1b98a44561f80d2a2e03e4548e830e9d
Clay Ellis <clayellis@me.com>
2017-11-21 12:47:51 -0700

    Enable TaskListViewController to send and receive editing events to TaskListViewModel

--- Sources/Views/Lists/List/TaskListViewController.swift
+++ Sources/Views/Lists/List/TaskListViewController.swift
@@ -41,20 +41,37 @@ final class TaskListViewController: UIViewController {
 
     func configureNavigationBar() {
         title = viewModel.title
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Edit", style: .plain, target: self, action: #selector(rightButtonTapped(_:)))
     }
 
     func observeViewModel() {
         viewModel.dataDidChange = { [weak self] in
             self?.taskListView.tableView.reloadData()
         }
+
+        viewModel.editingDidChange = { [weak self] editing in
+            guard let rightButton = self?.navigationItem.rightBarButtonItem else { return }
+            if editing {
+                rightButton.title = "Done"
+            } else {
+                rightButton.title = "Edit"
+            }
+        }
+    }
+
+    @objc func rightButtonTapped(_ button: UIBarButtonItem) {
+        viewModel.toggleEditing()
     }
 }
 
 extension TaskListViewController: UITableViewDataSource, UITableViewDelegate {
     func configure(tableView: UITableView) {
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: UITableViewCell.reuseIdentifier)
+//        tableView.register(UITableViewCell.self, forCellReuseIdentifier: UITableViewCell.reuseIdentifier)
+        tableView.register(TaskListCell.self, forCellReuseIdentifier: TaskListCell.reuseIdentifier)
         tableView.dataSource = self
         tableView.delegate = self
+        tableView.keyboardDismissMode = .onDrag
     }
 
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -66,17 +83,65 @@ extension TaskListViewController: UITableViewDataSource, UITableViewDelegate {
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.numberOfTasks(in: section)
+        return viewModel.numberOfItems(in: section)
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 44
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: UITableViewCell.reuseIdentifier, for: indexPath)
-        cell.textLabel?.text = viewModel.titleForTask(at: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: TaskListCell.reuseIdentifier, for: indexPath) as! TaskListCell
+        cell.textView.delegate = self
+        cell.textView.text = viewModel.titleForTask(at: indexPath)
+        if viewModel.indexPathRepresentsNewTaskRow(indexPath) {
+            cell.textView.isEditable = true
+            cell.textView.isUserInteractionEnabled = true
+        } else {
+            cell.textView.isEditable = false
+            cell.textView.isUserInteractionEnabled = false
+        }
         return cell
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        viewModel.toggleTaskCompletionStatus(at: indexPath)
+        guard let cell = tableView.cellForRow(at: indexPath) as? TaskListCell else { return }
+        if viewModel.indexPathRepresentsNewTaskRow(indexPath) {
+            cell.textView.becomeFirstResponder()
+        } else {
+            viewModel.toggleTaskCompletionStatus(at: indexPath)
+        }
+    }
+}
+
+extension TaskListViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        viewModel.beginCreatingNewTask()
+    }
+
+    func textViewDidChange(_ textView: UITextView) {
+        viewModel.updateNewTask(title: textView.text)
+    }
+
+    func textViewDidEndEditing(_ textView: UITextView) {
+        viewModel.commitNewTask()
+    }
+
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if text == "\n" {
+            endEditing()
+            return false
+        } else {
+            return true
+        }
+    }
+
+    func endEditing() {
+        view.endEditing(true)
     }
 }
