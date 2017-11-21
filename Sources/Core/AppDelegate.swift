e62f36f3cec2f627b24eec5a7941555f8bce113e
Clay Ellis <clayellis@me.com>
2017-11-20 22:26:20 -0700

    Make window root the carousel

--- Sources/Core/AppDelegate.swift
+++ Sources/Core/AppDelegate.swift
@@ -14,9 +14,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        let container = DependencyContainer()
+//        let dataPopulator = container.makeDataPopulator()
+//        dataPopulator.populateData()
+
         // TODO: Delegate window and view state to AppCoordinator
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = ListsViewController()
+        let carousel = container.makeTaskListCarouselController()
+        let navigationController = BaseNavigationController(rootViewController: carousel)
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
         return true
     }
