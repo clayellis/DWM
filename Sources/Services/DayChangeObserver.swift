a002cffeda8237cfa43c4d06b4c3743d9944bf1a
Clay Ellis <clayellis@me.com>
2017-11-23 20:17:26 -0800

    DayChangeObserver now uses identifiers when starting and stopping observation and executes the closure passed on starting

--- Sources/Services/DayChangeObserver.swift
+++ Sources/Services/DayChangeObserver.swift
@@ -8,25 +8,29 @@
 
 import Foundation
 
+// TODO: Create a simulateByNotification method that will post a NSCalendarDayChanged notification
+
 /// Defines an observer that watches for day changes
 protocol DayChangeObserverProtocol: class {
-    /// Starts observing day changes
-    func startObserving()
+    /// Starts observing day changes with an `identifier` and executes `closure` when changes occur
+    /// - parameter identifier: An identifier used when stopping observation (see `stopObserving(identifier:)`)
+    /// - parameter closure: The closure called when changes are observed
+    func startObserving(identifier: String, _ closure: @escaping () -> ())
 
-    /// Stops observing day changes
-    func stopObserving()
+    /// Stops observing day changes with an `identifier`
+    /// - parameter identifier: The identifier used to start observing (see `startObserving(identifier:_:)`)
+    func stopObserving(identifier: String)
 
-    /// Returns `true` if the day has changed since the last check, otherwise returns `false`
+    /// Returns `true` if the day has changed since the last check and triggers the observation closure passed in `startObserving`.
+    /// Otherwise returns `false`
     func checkForChanges() -> Bool
-
-    /// Closure which is executed when changes are observed
-    var onChangesObserved: (() -> ())? { get set }
 
     /// Resets the observer such that `checkForChanges()` will return `false` even if the day has changed
     func reset()
 }
 
 class DayChangeObserver: DayChangeObserverProtocol {
+    typealias Observer = () -> ()
 
     struct Keys {
         static var lastObservedDate = "LastObservedDate"
@@ -34,10 +38,12 @@ class DayChangeObserver: DayChangeObserverProtocol {
 
     let storage: SimpleStoreProtocol
     let timeEngine: TimeEngineProtocol
+    private var observers: [String: Observer]
 
     init(storage: SimpleStoreProtocol = UserDefaults.standard, timeEngine: TimeEngineProtocol) {
         self.storage = storage
         self.timeEngine = timeEngine
+        observers = [:]
     }
 
     deinit {
@@ -64,15 +70,17 @@ class DayChangeObserver: DayChangeObserverProtocol {
 
     // MARK: Protocol
 
-    var onChangesObserved: (() -> ())? = nil
-
-    func startObserving() {
+    func startObserving(identifier: String, _ closure: @escaping () -> ()) {
         NotificationCenter.default.addObserver(self, selector: #selector(changesObserved), name: .NSCalendarDayChanged, object: nil)
+        observers[identifier] = closure
         _ = checkForChanges()
     }
 
-    func stopObserving() {
-        NotificationCenter.default.removeObserver(self)
+    func stopObserving(identifier: String) {
+        observers.removeValue(forKey: identifier)
+        if observers.isEmpty {
+            NotificationCenter.default.removeObserver(self)
+        }
     }
 
     func checkForChanges() -> Bool {
@@ -85,12 +93,22 @@ class DayChangeObserver: DayChangeObserverProtocol {
     }
 
     @objc private func changesObserved() {
-        DispatchQueue.main.async {
-            self.onChangesObserved?()
+//        DispatchQueue.main.async {
+        for observer in observers.values {
+            observer()
         }
+//        }
     }
 
     func reset() {
         lastObservedDate = nil
+    }
+
+    // MARK: Helpers
+
+    func stopObserving() {
+        for identifier in observers.keys {
+            stopObserving(identifier: identifier)
+        }
     }
 }
