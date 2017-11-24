4fdedfac0c6d338257e261db2fc6959fe88c51e4
Clay Ellis <clayellis@me.com>
2017-11-23 20:22:10 -0800

    Stop observing notifications in view did disappear

--- Sources/Views/Lists/List/TaskListViewController.swift
+++ Sources/Views/Lists/List/TaskListViewController.swift
@@ -55,8 +55,8 @@ final class TaskListViewController: UIViewController {
         observeNotifications()
     }
 
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
         stopObservingNotifications()
     }
 
