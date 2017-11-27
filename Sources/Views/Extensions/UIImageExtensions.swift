ce1d08df57dd61df965f83fb6808df25554b3009
Clay Ellis <clayellis@me.com>
2017-11-27 07:38:56 -0700

    Create UIImageExtension for initializing and image with a color and adjusting its alpha

---
+++ Sources/Views/Extensions/UIImageExtensions.swift
@@ -0,0 +1,31 @@
+//
+// UIImage+Extras.swift
+// Clay Ellis
+// https://gist.github.com/clayellis/3c3bfdee9cac8e9b12ecaefb103d916b
+//
+
+import UIKit
+
+extension UIImage {
+    public convenience init?(color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) {
+        let rect = CGRect(origin: .zero, size: size)
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0)
+        let context = UIGraphicsGetCurrentContext()
+        context?.setFillColor(color.cgColor)
+        context?.fill(rect)
+
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        guard let cgImage = image?.cgImage else { return nil }
+        self.init(cgImage: cgImage)
+    }
+
+    func with(alpha value: CGFloat) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(size, false, scale)
+        draw(at: .zero, blendMode: .normal, alpha: value)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage!
+    }
+}
