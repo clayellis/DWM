4b2a534ea1d17129445834a18d75421314400033
Clay Ellis <clayellis@me.com>
2017-11-27 11:58:46 -0700

    Create FeedbackManager to manage feedback generators for certain actions

---
+++ Sources/Views/Lists/List/FeedbackManager.swift
@@ -0,0 +1,69 @@
+//
+//  FeedbackManager.swift
+//  DWM
+//
+//  Created by Clay Ellis on 11/27/17.
+//  Copyright © 2017 Test. All rights reserved.
+//
+
+import UIKit
+
+protocol FeedbackManagerProtocol {
+    func triggerCompletionTouchDown()
+    func cancelCompletionTouchDown()
+    func triggerCompletionTouchUp()
+    func triggerListChange()
+}
+
+class FeedbackManager: FeedbackManagerProtocol {
+    private lazy var queue: DispatchQueue = .main
+    private var completionTouchDownItem: DispatchWorkItem?
+    private var completionTouchDownTime: Date?
+
+    func triggerCompletionTouchDown() {
+        completionTouchDownTime = Date()
+        let generator = UIImpactFeedbackGenerator(style: .light)
+        generator.prepare()
+        completionTouchDownItem = DispatchWorkItem {
+            generator.impactOccurred()
+            self.completionTouchDownItem?.cancel()
+            self.completionTouchDownItem = nil
+        }
+        queue.asyncAfter(deadline: .now() + 0.05, execute: completionTouchDownItem!)
+    }
+
+    func cancelCompletionTouchDown() {
+        completionTouchDownItem?.cancel()
+        completionTouchDownItem = nil
+        completionTouchDownTime = nil
+    }
+
+    func triggerCompletionTouchUp() {
+        var impactStyle = UIImpactFeedbackStyle.medium
+        if let time = completionTouchDownTime {
+            let delta = Date().timeIntervalSince(time)
+            if delta > 0.5 {
+                impactStyle = .heavy
+            } else if delta > 0.2 {
+                impactStyle = .medium
+            } else {
+                impactStyle = .light
+            }
+            completionTouchDownTime = nil
+        }
+
+        if let item = completionTouchDownItem {
+            item.perform()
+            item.cancel()
+        }
+
+        // Delay the touch up slightly so that both are felt
+        let generator = UIImpactFeedbackGenerator(style: impactStyle)
+        generator.prepare()
+        queue.asyncAfter(deadline: .now() + 0.1) { generator.impactOccurred() }
+    }
+
+    func triggerListChange() {
+        UISelectionFeedbackGenerator().selectionChanged()
+    }
+}
