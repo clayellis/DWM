73f4ddeab3b064d92d91600d91d8f75f0a418327
Clay Ellis <clayellis@me.com>
2017-11-29 12:24:36 -0700

    Extend UIButton to return backgroundColor for state

--- Sources/Views/Extensions/UIButtonExtensions.swift
+++ Sources/Views/Extensions/UIButtonExtensions.swift
@@ -14,6 +14,14 @@ extension UIButton {
         self.setBackgroundImage(UIImage(color: color), for: state)
     }
 
+    /// Returns the background color used for a button state.
+    /// - parameter state: The state that uses the background color. Possible values are described in UIControlState.
+    /// - returns: The background color used for the specified state.
+    public func backgroundColor(for state: UIControlState) -> UIColor? {
+        let image = backgroundImage(for: state)
+        return image?.color(at: .zero)
+    }
+
     /// Adds `space` between the button's `imageView` and `titleLabel`
     /// with the `imageView` on the left.
     func setImageTitleSpacing(_ space: CGFloat) {
