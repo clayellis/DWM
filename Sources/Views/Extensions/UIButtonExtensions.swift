e39266431e7b6ff789577be7af624967fd27fb4d
Clay Ellis <clayellis@me.com>
2017-11-29 11:21:59 -0700

    Extend UIButton to set spacing between image and title

--- Sources/Views/Extensions/UIButtonExtensions.swift
+++ Sources/Views/Extensions/UIButtonExtensions.swift
@@ -9,8 +9,18 @@
 import UIKit
 
 extension UIButton {
-        /// Set the button's background color for a control state
-        public func setBackgroundColor(_ color: UIColor, forUIControlState state: UIControlState) {
-            self.setBackgroundImage(UIImage(color: color), for: state)
-        }
+    /// Set the button's background color for a control state
+    public func setBackgroundColor(_ color: UIColor, forUIControlState state: UIControlState) {
+        self.setBackgroundImage(UIImage(color: color), for: state)
+    }
+
+    /// Adds `space` between the button's `imageView` and `titleLabel`
+    /// with the `imageView` on the left.
+    func setImageTitleSpacing(_ space: CGFloat) {
+        let inset = space / 2
+        imageEdgeInsets.left = -inset
+        imageEdgeInsets.right = inset
+        titleEdgeInsets.left = inset
+        titleEdgeInsets.right = -inset
+    }
 }
