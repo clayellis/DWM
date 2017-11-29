d79cef7b1aa36598b62effc7cc2352f81c5c9d9a
Clay Ellis <clayellis@me.com>
2017-11-29 11:21:23 -0700

    Add UIImage convenience initializer to render a label as an image

--- Sources/Views/Extensions/UIImageExtensions.swift
+++ Sources/Views/Extensions/UIImageExtensions.swift
@@ -13,12 +13,10 @@ extension UIImage {
         let context = UIGraphicsGetCurrentContext()
         context?.setFillColor(color.cgColor)
         context?.fill(rect)
-
         let image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
-
         guard let cgImage = image?.cgImage else { return nil }
-        self.init(cgImage: cgImage)
+        self.init(cgImage: cgImage, scale: UIScreen.main.scale, orientation: .up)
     }
 
     func with(alpha value: CGFloat) -> UIImage {
@@ -27,5 +25,25 @@ extension UIImage {
         let newImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         return newImage!
+    }
+
+    /// Initialize a `UIImage` with a `UILabel` rendered inside.
+    /// - parameter label: The label to render.
+    /// - parameter sizeByContent: If `true`, the size of the resulting image will be
+    ///     the `instrinsicContentSize` of the `label`. Otherwise, the `label`'s `bounds`
+    ///     are used to determine the size. Defaults to `true`.
+    convenience init?(label: UILabel, sizeByContent: Bool = true) {
+        if sizeByContent {
+            label.frame.size = label.intrinsicContentSize
+            UIGraphicsBeginImageContextWithOptions(label.intrinsicContentSize, false, 0.0)
+        } else {
+            UIGraphicsBeginImageContextWithOptions(label.bounds.size, false, 0.0)
+        }
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        label.layer.render(in: context)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        guard let cgImage = image?.cgImage else { return nil }
+        self.init(cgImage: cgImage, scale: UIScreen.main.scale, orientation: .up)
     }
 }
