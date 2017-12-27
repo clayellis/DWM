8bb20c9941213dd3ee35415b9e2282fa6de83c65
Clay Ellis <clayellis@me.com>
2017-12-27 16:09:09 -0700

    DataPopulator: Create long list of items

--- Sources/Core/DataPopulator.swift
+++ Sources/Core/DataPopulator.swift
@@ -23,12 +23,16 @@ class DataPopulator {
 
     func populateData() {
         clearData()
-        let d1 = Task(title: "Daily One", frequency: .daily)
-        let d2 = Task(title: "Daily Two", frequency: .daily)
-        let d3 = Task(title: "Daily Three", frequency: .daily)
-        let d4 = Task(title: "Daily Four", frequency: .daily)
-        let d5 = Task(title: "Daily Five", frequency: .daily)
-        let d6 = Task(title: "Daily Six", frequency: .daily)
+        for n in 1...28 {
+            let task = Task(title: "Daily \(n)", frequency: .daily)
+            taskManager.createTask(task)
+        }
+//        let d1 = Task(title: "Daily One", frequency: .daily)
+//        let d2 = Task(title: "Daily Two", frequency: .daily)
+//        let d3 = Task(title: "Daily Three", frequency: .daily)
+//        let d4 = Task(title: "Daily Four", frequency: .daily)
+//        let d5 = Task(title: "Daily Five", frequency: .daily)
+//        let d6 = Task(title: "Daily Six", frequency: .daily)
 
         let w1 = Task(title: "Weekly One", frequency: .weekly)
         let w2 = Task(title: "Weekly Two", frequency: .weekly)
@@ -36,7 +40,8 @@ class DataPopulator {
         let m1 = Task(title: "Monthly One", frequency: .monthly)
         let m2 = Task(title: "Montly Two", frequency: .monthly)
 
-        [d1, d2, d3, d4, d5, d6, w1, w2, m1, m2].forEach(taskManager.createTask)
+//        [d1, d2, d3, d4, d5, d6, w1, w2, m1, m2].forEach(taskManager.createTask)
+        [w1, w2, m1, m2].forEach(taskManager.createTask)
     }
 
     func clearData() {
